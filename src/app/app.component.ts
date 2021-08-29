5e7d465b029dbf995f8d798405ea36af7868979e
sonali sonawane <sonawanesonali41@gmail.com>
2021-08-29 18:52:44 +0530

    typescript files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
 import { Component } from '@angular/core';
-
+import { Todo } from './todo'
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'first-angular-app';
+  title = 'Sonali Project';
+
+  todoValue: string="";
+  list:Todo[]=[];
+
+  ngONInit(){
+    this.list = []
+    this.todoValue="";
+  }
+  addItem(){
+    if(this.todoValue !=""){
+      const newItem :Todo ={
+        id:Date.now(),
+        value:this.todoValue,
+        isDone:false
+      };
+      this.list.push(newItem)
+    }
+    this.todoValue = "";
+  }
+  deleteItem(id:number){
+this.list = this.list.filter(item=>item.id!==id);
+}
 }
